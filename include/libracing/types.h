d15e0465804e977a35e8ab401109df0b965e43b5
Yauheni Chaburanau <chaburanau@proton.me>
2025-07-15 20:27:18 +0200

    Types: Separate Header file

---
+++ include/libracing/types.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef struct {
+    uint32_t size;
+    char *data;
+} string_t;
+
+typedef struct {
+    uint32_t size;
+    int32_t *data;
+} int32_array_t;
+
+typedef struct {
+    uint32_t size;
+    uint16_t *data;
+} uint16_array_t;
+
+typedef struct {
+    uint32_t size;
+    string_t *data;
+} string_array_t;
+
