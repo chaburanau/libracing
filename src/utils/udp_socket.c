fa328543c199757ecc7fabfc61f3aed055224b96
Yauheni Chaburanau <chaburanau@proton.me>
2025-09-07 22:02:48 +0200

    Utils: UDP Socket Error Handling

--- src/utils/udp_socket.c
+++ src/utils/udp_socket.c
@@ -1,96 +1,127 @@
-#include <stdlib.h>
+#include <stdbool.h>
 #include <stdint.h>
-#include <ws2tcpip.h>
-#include <winsock2.h>
+#include <stdlib.h>
 #include <winerror.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#include "./wsa_client.c"
 
 #pragma comment(lib, "Ws2_32.lib")
 
-int udp_socket_address_size = sizeof(SOCKADDR_IN);
+static int32_t udp_socket_address_size = sizeof(SOCKADDR_IN);
+static int32_t udp_socket_last_error = 0;
 
-static WSADATA udp_socket_wsa_data;
-static int udp_socket_wsa_users = 0;
-
-typedef struct UDPSocket {
-   SOCKET      _socket;
-   SOCKADDR_IN _address;
+typedef struct {
+  SOCKADDR_IN _address;
+  SOCKET _socket;
+  bool _wsa_initialized;
 } udp_socket_t;
 
-udp_socket_t *udp_socket_create(const char* address, int port) {
-   udp_socket_t *udp_socket = malloc(sizeof(udp_socket_t));
-   if (udp_socket == NULL) return NULL;
+udp_socket_t *udp_socket_create(const char *address, uint16_t port);
+bool udp_socket_destroy(udp_socket_t *udp_socket);
+int32_t udp_socket_send(udp_socket_t *udp_socket, char *data, int32_t size);
+int32_t udp_socket_receive(udp_socket_t *udp_socket, char *data, int32_t size);
+int32_t udp_socket_get_last_error(void);
 
-   if (udp_socket_wsa_users == 0) {
-      const int wsa_startup_status = WSAStartup(MAKEWORD(2, 2), &udp_socket_wsa_data);
-      if (wsa_startup_status != 0) {
-         free(udp_socket);
-         return NULL;
-      }
-   }
+udp_socket_t *udp_socket_create(const char *address, uint16_t port) {
+  udp_socket_t *udp_socket = malloc(sizeof(udp_socket_t));
+  if (udp_socket == NULL) {
+    udp_socket_last_error = -1;
+    return NULL;
+  }
 
-   udp_socket_wsa_users++;
+  ZeroMemory(&udp_socket->_address, udp_socket_address_size);
+  udp_socket->_address.sin_family = AF_INET;
+  udp_socket->_address.sin_port = htons(port);
+  udp_socket->_socket = INVALID_SOCKET;
+  udp_socket->_wsa_initialized = false;
 
-   udp_socket->_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-   if (udp_socket->_socket == INVALID_SOCKET) {
-      free(udp_socket);
-      return NULL;
-   }
+  const int32_t wsa_status = wsa_client_add_user();
+  if (wsa_status != 0) {
+    udp_socket_last_error = wsa_status;
+    udp_socket_destroy(udp_socket);
+    return NULL;
+  }
 
-   ZeroMemory(&udp_socket->_address, udp_socket_address_size);
-   udp_socket->_address.sin_family = AF_INET;
-   udp_socket->_address.sin_port = htons(port);
+  udp_socket->_wsa_initialized = true;
 
-   const int32_t inet_pton_status = inet_pton(AF_INET, address, &udp_socket->_address.sin_addr);
-   if (inet_pton_status != 1) {
-      free(udp_socket);
-      return NULL;
-   }
+  udp_socket->_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (udp_socket->_socket == INVALID_SOCKET) {
+    udp_socket_last_error = WSAGetLastError();
+    udp_socket_destroy(udp_socket);
+    return NULL;
+  }
 
-   return udp_socket;
+  const int32_t inet_pton_status = inet_pton(AF_INET, address, &udp_socket->_address.sin_addr);
+  if (inet_pton_status != 1) {
+    udp_socket_last_error = WSAGetLastError();
+    udp_socket_destroy(udp_socket);
+    return NULL;
+  }
+
+  return udp_socket;
 }
 
-void udp_socket_destroy(udp_socket_t *udp_socket) {
-   if (udp_socket == NULL) return;
-   if (udp_socket->_socket == INVALID_SOCKET) return;
+bool udp_socket_destroy(udp_socket_t *udp_socket) {
+  if (udp_socket == NULL) {
+    return false;
+  }
 
-   closesocket(udp_socket->_socket);
-   free(udp_socket);
+  if (udp_socket->_socket != INVALID_SOCKET) {
+    const int32_t status = closesocket(udp_socket->_socket);
+    if (status != 0) {
+      udp_socket_last_error = WSAGetLastError();
+      return true;
+    }
+  }
 
-   udp_socket_wsa_users--;
-   if (udp_socket_wsa_users == 0) {
-      WSACleanup();
-      ZeroMemory(&udp_socket_wsa_data, sizeof(WSADATA));
-   }
+  if (udp_socket->_wsa_initialized == true) {
+    const int32_t wsa_status = wsa_client_remove_user();
+    if (wsa_status != 0) {
+      udp_socket_last_error = wsa_status;
+      return true;
+    }
+  }
+
+  free(udp_socket);
+  return false;
 }
 
-int32_t udp_socket_send(udp_socket_t *udp_socket, char *data, int size) {
-   if (udp_socket_wsa_users == 0) return -1;
-   if (udp_socket == NULL) return -1;
-
-   int32_t bytes_sent = sendto(
-      udp_socket->_socket, data, size, 0,
-      (struct sockaddr *)&udp_socket->_address,
-      udp_socket_address_size);
-
-   if (bytes_sent == SOCKET_ERROR) return -1;
-   return bytes_sent;
+struct sockaddr* _address(udp_socket_t* udp_socket) {
+  return (struct sockaddr *)&udp_socket->_address;
 }
 
-int32_t udp_socket_receive(udp_socket_t *udp_socket, char *data, const int size) {
-   if (udp_socket_wsa_users == 0) return -1;
-   if (udp_socket == NULL) return -1;
+int32_t udp_socket_send(udp_socket_t *udp_socket, char *data, int32_t size) {
+  if (udp_socket == NULL) {
+    udp_socket_last_error = -1000;
+    return -1000;
+  }
 
-   const int32_t bytes_received = recvfrom(
-      udp_socket->_socket, data, size, 0,
-      (struct sockaddr *)&udp_socket->_address,
-      &udp_socket_address_size);
+  const int32_t bytes_sent = sendto(udp_socket->_socket, data, size, 0, _address(udp_socket), udp_socket_address_size);
+  if (bytes_sent == SOCKET_ERROR) {
+    udp_socket_last_error = WSAGetLastError();
+    return -1;
+  }
 
-   if (bytes_received == SOCKET_ERROR) return -1;
-   return bytes_received;
+  return bytes_sent;
 }
 
-int32_t udp_socket_get_last_error(udp_socket_t *udp_socket) {
-   if (udp_socket_wsa_users == 0) return -1;
-   if (udp_socket == NULL) return -1;
-   return WSAGetLastError();
+int32_t udp_socket_receive(udp_socket_t *udp_socket, char *data, int32_t size) {
+  if (udp_socket == NULL) {
+    udp_socket_last_error = -1;
+    return -1;
+  }
+
+  const int32_t bytes_received = recvfrom(udp_socket->_socket, data, size, 0, _address(udp_socket), &udp_socket_address_size);
+  if (bytes_received == SOCKET_ERROR) {
+    udp_socket_last_error = WSAGetLastError();
+    return -1;
+  }
+
+  return bytes_received;
+}
+
+int32_t udp_socket_get_last_error(void) {
+  return udp_socket_last_error;
 }
