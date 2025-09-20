82b1a475fb3a1797c818a9f1fb1028d7f8109310
Yauheni Chaburanau <chaburanau@proton.me>
2025-09-20 20:24:29 +0200

    Utils: Types macro changes

--- src/utils/types.c
+++ src/utils/types.c
@@ -1,43 +1,55 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define DEFINE_ARRAY_TYPE(TYPE, NAME)                \
-    typedef struct {                                 \
-        TYPE *data;                                  \
-        size_t size;                                 \
-    } NAME;                                          \
-                                                     \
-    static inline NAME *NAME##_create(size_t size) { \
-        NAME *array = malloc(sizeof(NAME));          \
-        if (array == NULL) {                         \
-            return NULL;                             \
-        }                                            \
-                                                     \
-        array->data = malloc(sizeof(TYPE) * size);   \
-        if (array->data == NULL) {                   \
-            free(array);                             \
-            return NULL;                             \
-        }                                            \
-                                                     \
-        array->size = size;                          \
-        return array;                                \
-    }                                                \
-                                                     \
-    static inline void NAME##_destroy(NAME *array) { \
-        if (array == NULL) {                         \
-            return;                                  \
-        }                                            \
-                                                     \
-        free(array->data);                           \
-        free(array);                                 \
-        array = NULL;                                \
+#define DEFINE_ARRAY_TYPE(TYPE, NAME)              \
+    typedef struct {                               \
+        TYPE *data;                                \
+        size_t size;                               \
+    } NAME;                                        \
+                                                   \
+    bool NAME##_init(NAME *array, size_t size) {  \
+        if (array == NULL) {                       \
+            return true;                           \
+        }                                          \
+                                                   \
+        array->size = size;                        \
+        array->data = malloc(sizeof(TYPE) * size); \
+                                                   \
+        if (array->data == NULL) {                 \
+            return false;                          \
+        }                                          \
+                                                   \
+        return true;                               \
+    }                                              \
+                                                   \
+    void NAME##_free(NAME *array) {                \
+        if (array == NULL) {                       \
+            return;                                \
+        }                                          \
+        if (array->data != NULL) {                 \
+            free(array->data);                     \
+            array->data = NULL;                    \
+        }                                          \
     }
 
 DEFINE_ARRAY_TYPE(char, string_t)
 DEFINE_ARRAY_TYPE(string_t, strings_t)
 DEFINE_ARRAY_TYPE(int32_t, int32s_t)
 DEFINE_ARRAY_TYPE(uint16_t, uint16s_t)
+
+void strings_free(strings_t *array) {
+    if (array == NULL || array->data == NULL) {
+        return;
+    }
+
+    for (size_t index = 0; index < array->size; index++) {
+        string_t_free(&array->data[index]);
+    }
+
+    strings_t_free(array);
+}
